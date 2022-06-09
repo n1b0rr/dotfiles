ca02560086da72d14386594cc8f1511d8ab75fd5
n1b0rr <robin.schellemans174@gmail.com>
2022-06-09 18:24:14 +0200

    isntallation of zsh

--- arch-i3gaps/base-install.sh
+++ arch-i3gaps/base-install.sh
@@ -3,9 +3,13 @@
 mkdir instalation
 sudo pacman -S xorg-server i3-gaps xorg-apps xorg-xinit i3blocks i3lock alacritty
 git clone --recurse-submodules https://github.com/nullgemm/ly
+cd instalation
 cd ly
 make
 sudo make install
 sudo systemctl enable ly.service
 cd ..
+sudo pacman -S zsh
+chsh -s /bin/zsh
+
 
