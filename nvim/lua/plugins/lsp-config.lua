8a779425b1c0d5da33cd9e04a3288b4cf2e68ca5
n1b0rr <robin.schellemans174@gmail.com>
2024-04-16 18:43:35 +0200

    moved nvim config files

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        config = function()
+            require("mason-lspconfig").setup({
+            ensure_installed = {"lua_ls", "clangd", "jdtls", "jsonls", "html", "tsserver", "pyright"} -- clangd = C, jdtls = Java
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.jdtls.setup({
+                root_dir = lspconfig.util.root_pattern('.git','src/');})
+            lspconfig.jsonls.setup({})
+            lspconfig.html.setup({})
+            lspconfig.tsserver.setup({})
+            lspconfig.pyright.setup({})
+
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}
