70da3e18e9b09b9a65e6f7ff608cba88b19ef5d7
n1b0rr <robin.schellemans174@gmail.com>
2024-05-15 08:50:01 +0200

    support for latex and terminal

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         'williamboman/mason-lspconfig.nvim',
         config = function()
             require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls", "clangd", "jdtls", "jsonls", "html", "tsserver", "pyright"} -- clangd = C, jdtls = Java
+            ensure_installed = {"lua_ls", "clangd", "jdtls", "jsonls", "html", "tsserver", "pyright", "ltex"} -- clangd = C, jdtls = Java
             })
         end
     },
@@ -24,6 +24,7 @@ return {
             lspconfig.jsonls.setup({})
             lspconfig.html.setup({})
             lspconfig.tsserver.setup({})
+            lspconfig.pyright.setup({})
             lspconfig.pyright.setup({})
 
 
